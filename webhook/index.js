11e2529c24248dbaea8f639c1dd40c830fc7c38d
蒋东 <852778359@qq.com>
2021-02-07 17:39:15 +0800

    test: hook

--- webhook/index.js
+++ webhook/index.js
@@ -1,11 +1,17 @@
 const Koa = require('koa');
 const app = new Koa();
+const child_process = require('child_process'); 
 // const createHandler = require("github-webhook-handler");
 
 
 app.use(async (ctx, next) => {    //调用koa2的use方法来创建一个上下文  
 	if (ctx.request.path === '/webhook') {
-		console.log(ctx);
+		// console.log(ctx);
+		// TODO:校验秘钥
+		// if (!ctx.request.header['x-hub-signature']) {
+		// 	console.log('错误：webhook secret在github未配置或者获取签名失败');
+		// }
+		child_process.execFile('./script.sh')
 	} else {
 		await next();
 	}
--- webhook/script.sh
+++ webhook/script.sh
@@ -1,13 +1,5 @@
 # 更新同步代码
-# WEB_PATH='/root/githubWebhook/warehouse/my-Gatsby-Blog'
-
-
-# echo "开始执行shell"
-# cd $WEB_PATH
-# echo "pulling source code..."
-# git pull
-# echo "changing permissions..."
-# #chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
-# echo " git pull 完成. 开始 build"
-# yarn run gatsby build
-# echo "build 完成"
+echo "开始执行shell"
+echo "pulling source code..."
+git pull
+echo " git pull 完成. 开始 重新启动服务"
