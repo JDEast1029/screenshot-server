536a9135c6f551dedc1f0452c58e23c9af0bc137
蒋东 <852778359@qq.com>
2021-02-08 10:04:11 +0800

    fix: 更改pm2配置项

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,15 @@ module.exports = {
 			name: 'webhook',
 			script: 'webhook/index.js',
 			watch: true,
+			ignore_watch: ['node_modules', 'logs'],  // 不用监听的文件    
+			error_file: "logs/webhook/err.log",         // 错误日志文件    
+			out_file: "logs/webhook/out.log",           // 正常日志文件    
+			merge_logs: true,                         // 设置追加日志而不是新建日志    
+			log_date_format: "YYYY-MM-DD HH:mm:ss",   // 指定日志文件的时间格式    
+			min_uptime: "60s",                        // 应用运行少于时间被认为是异常启动    
+			max_restarts: 30,                         // 最大异常重启次数，即小于min_uptime运行时间重启次数；    
+			autorestart: true,                        // 默认为true, 发生异常的情况下自动重启    
+			restart_delay: 4000,                     // 异常重启情况下，延时重启时间 单位ms  
 		},
 		// 截图服务
 		{
@@ -21,8 +30,8 @@ module.exports = {
 			exec_mode: "cluster_mode",                // 应用启动模式，支持fork和cluster模式    
 			instances: 4,                             // 应用启动实例个数，仅在cluster模式有效 默认为fork；或者 max    
 			max_memory_restart: '1G',                 // 最大内存限制数，超出自动重启    
-			error_file: "logs/app-err.log",         // 错误日志文件    
-			out_file: "logs/app-out.log",           // 正常日志文件    
+			error_file: "logs/app/err.log",         // 错误日志文件    
+			out_file: "logs/app/out.log",           // 正常日志文件    
 			merge_logs: true,                         // 设置追加日志而不是新建日志    
 			log_date_format: "YYYY-MM-DD HH:mm:ss",   // 指定日志文件的时间格式    
 			min_uptime: "60s",                        // 应用运行少于时间被认为是异常启动    
